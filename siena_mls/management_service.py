eadcf7253e348224f7a82c335d33f4699e55cd09
Ninad P.Chaudhari <ninadpchaudhari@gmail.com>
2024-01-24 16:32:16 -0500

    Create management_service.py

---
+++ siena_mls/management_service.py
@@ -0,0 +1,33 @@
+import requests
+import warnings
+from warnings import DeprecationWarning
+import sys
+from importlib import metadata # To enable getting MES version using __version__
+class ManagementService: 
+  
+  def __init__(self):
+    self.__version__ = metadata.version("Siena-MLS")
+    self.pyVersion = 
+
+  def get_latest_version(self, package_name):
+    response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+    data = response.json()
+    return data["info"]["version"]
+    
+  def get_installed_version(self, package_name):
+    response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+    data = response.json()
+    return data["info"]["version"]
+
+  def printVersion(self):
+    print(`Current MLS Version is ${self.__version})
+    print(sys.version)
+    
+  def getVersion(self):
+    return self.__version__
+    
+  def notify_if_outdated(self):
+    installed_version = self.__version__
+    latest_version = get_latest_version(package_name)
+    if latest_version > installed_version:
+      warnings.warn(`Friently Nudge: You are using ${installed_version} of Siena-mls`, DeprecationWarning)
