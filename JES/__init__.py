48c45d09fa0dbb187bdb8a14ea94efa9f6b84399
Ninad Chaudhari <nchaudhari@albany.edu>
2024-01-25 15:12:05 -0500

    add JES package as well

---
+++ JES/__init__.py
@@ -0,0 +1,1 @@
+from siena_mls import *
--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 from siena_mls import *
+# OR: from JES import *
 a = makePicture("tests/assets/siena-poster_resized_model.jpeg")
 addRectFilled(a, 10, 10, 100, 50, white)
 showImage(a)
